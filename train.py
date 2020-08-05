4a777cdd2cc7664ecff854f7c4407851f1a9d353
Pascal Perle <Pascal@FamiliePerle.de>
2020-08-05 23:25:52 +0200

    ✨ add data normalization for ImageNet

--- train.py
+++ train.py
@@ -1,5 +1,6 @@
 from data import Flickr8k
 from torchvision import transforms as T
 
-transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor()])  # TODO normalize
-dat = Flickr8k('Flicker8k_Dataset', 'Flickr_8k.trainImages.txt', 'Flickr8k.token.txt', transform=transform)
+normalize = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor(), normalize])
+data_train = Flickr8k('Flicker8k_Dataset', 'Flickr_8k.trainImages.txt', 'Flickr8k.token.txt', transform=transform)

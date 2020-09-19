31977acbcf3296a7dbd6f75321796cc20422926f
Pascal Perle <Pascal@FamiliePerle.de>
2020-09-19 10:12:12 +0200

    ✨ visualize attention

--- visualize.py
+++ visualize.py
@@ -1,14 +1,15 @@
 from datetime import datetime
+from typing import List
 
-import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
+from joeynmt.constants import BOS_TOKEN, EOS_TOKEN
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 
 from data import Flickr8k
 from model import Image2Caption
-
-matplotlib.use('TkAgg')
 
 
 class NormalizeInverse(transforms.Normalize):
@@ -33,6 +34,7 @@ normalize_inverse = NormalizeInverse(mean=[0.485, 0.456, 0.406], std=[0.229, 0.2
 
 class Tensorboard:
     def __init__(self, log_dir: str = f'runs/image_captioning_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}', image_idxs=[7, 42, 128, 512, 1337], device: str = 'cpu'):
+        self.log_dir = log_dir
         self.writer = SummaryWriter(log_dir)
         self.image_idxs = image_idxs
         self.device = device
@@ -48,7 +50,35 @@ class Tensorboard:
         for image_idx in self.image_idxs:
             img, _, _ = dataset[image_idx]
             img = img.unsqueeze(0).to(self.device)
-            prediction, _ = model.predict(dataset, img, max_output_length, beam_size, beam_alpha)
-            self.writer.add_text(f'image-{image_idx}',
-                                 '    ' + ' '.join(dataset.corpus.vocab.arrays_to_sentences(prediction)[0]), global_step)
+            prediction, attention_scores = model.predict(dataset, img, max_output_length, beam_size, beam_alpha)
+            decoded_prediction = dataset.corpus.vocab.arrays_to_sentences(prediction)[0]
+            self.writer.add_text(f'image-{image_idx}', '    ' + ' '.join(decoded_prediction), global_step)
+            visualize_attention(img.squeeze(0), decoded_prediction, attention_scores[0], dataset.max_length, f'{self.log_dir}/{image_idx}-step_{global_step:03d}.png')
         self.writer.flush()
+
+
+def visualize_attention(image: torch.Tensor, word_seq: List[str], attention_scores: np.ndarray, max_length: int, file_name: str):
+    image = normalize_inverse(image).cpu().detach().numpy()
+    image = (image.transpose((1, 2, 0)) * 225).astype(np.uint8)
+
+    height = int(np.ceil((max_length + 1) / 5.))
+    fig, ax = plt.subplots(height, 5, figsize=(height * 3, 15))
+    [axi.set_axis_off() for axi in ax.ravel()]  # hide axes for subplots
+
+    ax[0][0].set_title(BOS_TOKEN)
+    ax[0][0].imshow(image)
+
+    if len(word_seq) > 0:
+        if word_seq[-1] != EOS_TOKEN:
+            word_seq.append(EOS_TOKEN)
+
+        extent = [0, image.shape[0], image.shape[1], 0]
+        attention_score_shape = [np.round(np.sqrt(attention_scores.shape[-1])).astype(np.uint8)] * 2
+        for idx, (word, attention_score) in enumerate(zip(word_seq, attention_scores), start=1):
+            current_axis = ax[idx // 5][idx % 5]
+            current_axis.set_title(word)
+            current_axis.imshow(image)
+            current_axis.imshow(attention_score.reshape(attention_score_shape), cmap='hot', interpolation='bilinear', alpha=0.5, extent=extent, origin='upper')
+
+    fig.savefig(file_name)
+    plt.close(fig)

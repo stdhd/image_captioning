a1ff359162a0ae52f782d0c56ec2093485ebfb95
Stephan <44555785+stdhd@users.noreply.github.com>
2020-10-14 11:52:40 +0200

    remove pretrained_embeddings

--- train.py
+++ train.py
@@ -20,7 +20,6 @@ from tqdm import tqdm, trange
 from custom_decoder import CustomRecurrentDecoder
 from data import Flickr8k
 from model import Image2Caption, Encoder
-from pretrained_embeddings import PretrainedEmbeddings
 from visualize import Tensorboard
 from yaml_parser import parse_yaml
 
@@ -39,14 +38,12 @@ def clip_gradient(optimizer: Optimizer, grad_clip: float) -> None:
                 param.grad.data.clamp_(-grad_clip, grad_clip)
 
 
-def setup_model(params: dict, data: Flickr8k, pretrained_embeddings: PretrainedEmbeddings) -> Tuple[Embeddings, Image2Caption]:
+def setup_model(params: dict, data: Flickr8k) -> Tuple[Embeddings, Image2Caption]:
     """
     setup embeddings and seq2seq model
 
     :param params: params from the yaml file
     :param data: Flickr Dataset class
-    :param pretrained_embeddings: PretrainedEmbeddings if selected in yaml
-    :return: word embeddings, seq2seq model
     """
 
     def get_base_arch(encoder_name: str) -> Callable:
@@ -64,7 +61,7 @@ def setup_model(params: dict, data: Flickr8k, pretrained_embeddings: PretrainedE
             return getattr(models, encoder_name)
 
     encoder = Encoder(get_base_arch(params.get('encoder')), device, pretrained=True)
-    vocab_size = len(data.corpus.vocab.itos) if not params.get('embed_pretrained', False) else 300
+    vocab_size = len(data.corpus.vocab.itos)
 
     if params.get('decoder_type', 'RecurrentDecoder') == 'RecurrentDecoder':
         decoder_type = CustomRecurrentDecoder
@@ -73,7 +70,7 @@ def setup_model(params: dict, data: Flickr8k, pretrained_embeddings: PretrainedE
 
     decoder = decoder_type(
         rnn_type=params.get('rnn_type'),
-        emb_size=params['embed_size'] if not params.get('embed_pretrained', False) else 300,
+        emb_size=params['embed_size'],
         hidden_size=params['hidden_size'],
         encoder=encoder,
         vocab_size=vocab_size,
@@ -84,10 +81,7 @@ def setup_model(params: dict, data: Flickr8k, pretrained_embeddings: PretrainedE
         num_layers=params.get('decoder-num_layers', 1)
     )
 
-    if params.get('embed_pretrained', False):
-        embeddings = pretrained_embeddings
-    else:
-        embeddings = Embeddings(embedding_dim=params['embed_size'], vocab_size=vocab_size)
+    embeddings = Embeddings(embedding_dim=params['embed_size'], vocab_size=vocab_size)
 
     return embeddings, Image2Caption(encoder, decoder, embeddings, device, params['freeze_encoder'], params.get('fine_tuning', None), params.get('dropout_after_encoder', 0), params['hidden_size']).to(device)
 
@@ -127,8 +121,6 @@ if __name__ == '__main__':
 
     grad_clip = params.get('grad_clip', None)
 
-    embed_pretrained = params.get('embed_pretrained', False)
-
     normalize = T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     transform = T.Compose([T.Resize(256), T.CenterCrop(224), T.ToTensor(), normalize])
 
@@ -138,11 +130,6 @@ if __name__ == '__main__':
     else:
         data_train = Flickr8k('data/Flicker8k_Dataset', 'data/Flickr_8k.trainImages.txt', 'data/Flickr8k.token.txt', transform=transform, max_vocab_size=params['max_vocab_size'], all_lower=params['all_lower'])
 
-    if embed_pretrained:
-        pretrained_embeds = PretrainedEmbeddings("embeddings/glove_shrinked.txt", data_train.get_corpus(), device)
-    else:
-        pretrained_embeds = None
-
     dataloader_train = DataLoader(data_train, batch_size, shuffle=True, num_workers=os.cpu_count())  # set num_workers=0 for debugging
 
     data_dev = Flickr8k('data/Flicker8k_Dataset', 'data/Flickr_8k.devImages.txt', 'data/Flickr8k.token.txt', transform=transform, max_vocab_size=params['max_vocab_size'], all_lower=params['all_lower'])
@@ -150,15 +137,12 @@ if __name__ == '__main__':
     dataloader_dev = DataLoader(data_dev, batch_size, num_workers=os.cpu_count())  # os.cpu_count()
 
     decoder_type = params.get('decoder_type', 'RecurrentDecoder')
-    embeddings, model = setup_model(params, data_train, pretrained_embeds)
+    embeddings, model = setup_model(params, data_train)
 
     tensorboard = Tensorboard(log_dir=f'runs/{model_name}', device=device)
     tensorboard.add_images_with_ground_truth(data_dev)
 
-    if embed_pretrained:
-        criterion = nn.CosineEmbeddingLoss()
-    else:
-        criterion = nn.CrossEntropyLoss(ignore_index=data_train.corpus.vocab.stoi[PAD_TOKEN])
+    criterion = nn.CrossEntropyLoss(ignore_index=data_train.corpus.vocab.stoi[PAD_TOKEN])
 
     optimizer = optim.Adam(model.parameters(), lr=float(params['learning_rate']), weight_decay=float(params['weight_decay']))
     last_validation_score = float('-inf')
@@ -195,12 +179,8 @@ if __name__ == '__main__':
                                              unroll_steps=unroll_steps,
                                              decoder_type=decoder_type)
 
-            if embed_pretrained:
-                targets = labels[:, 1:unroll_steps].contiguous()
-                loss = criterion(outputs, pretrained_embeds(targets.long()), torch.tensor([1]).float().to(device))
-            else:
-                targets = labels[:, 1:unroll_steps].contiguous().view(-1)
-                loss = criterion(outputs.contiguous().view(-1, outputs.shape[-1]), targets.long())
+            targets = labels[:, 1:unroll_steps].contiguous().view(-1)
+            loss = criterion(outputs.contiguous().view(-1, outputs.shape[-1]), targets.long())
 
             if att_probs is not None:  # only with RecurrentDecoder, TransformerDecoder does not have attention
                 loss += 1. * ((1. - att_probs.sum(dim=1)) ** 2).mean()  # Doubly stochastic attention regularization
